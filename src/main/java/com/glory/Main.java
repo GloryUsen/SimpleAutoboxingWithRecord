e110ce5bf70a67cfb0e9ca123bcd4c782f94fd75
GloryUsen <ganwana89@gmail.com>
2025-05-09 17:01:13 +0100

    last update

--- src/main/java/com/glory/Main.java
+++ src/main/java/com/glory/Main.java
@@ -16,8 +16,8 @@ record Customer(String name, ArrayList<Double> transactions){ // Using record no
 public class Main {
     public static void main(String[] args) {
 
-        Customer glo = new Customer("gloria", 1000.0);
-        System.out.println(glo);
+        Customer gloria = new Customer("Gloria S", 1000.0);
+        System.out.println(gloria);
 
         // Creating a new bank and adding a new customer
 
@@ -25,6 +25,7 @@ public class Main {
 
         Bank bank = new Bank("Chase");
         bank.addNewCustomer("Glory U", 500.0);
+        bank.addNewCustomer("Gloria S", 1000.0);
         System.out.println(bank); //printing the bank object
 
         System.out.println("--------------");
@@ -32,6 +33,17 @@ public class Main {
         bank.addTransaction("Glory U", -10.25);
         bank.addTransaction("glory U", -75.01);
         bank.printStatement("Glory u");
+
+        // Customer wasn't added here
+//        bank.addTransaction("gloria", 100);
+//        bank.printStatement("gloria");
+
+        // Adding gloria as a new customer
+        System.out.println("-------------------");
+        bank.addTransaction("gloria s", 25);
+        bank.addTransaction("Gloria S", 100);
+        bank.printStatement("Gloria S");
+
 
 
 
